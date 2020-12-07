7e8ffaaa8a20cd6f50a2deb0dc5914580ae653fc
williamhuls <71887417+williamhuls@users.noreply.github.com>
2020-12-07 09:07:03 -0600

    Starter Code

---
+++ public/scripts/app.js
@@ -0,0 +1,79 @@
+let app;
+let map;
+let neighborhood_markers = 
+[
+    {location: [44.942068, -93.020521], marker: null},
+    {location: [44.977413, -93.025156], marker: null},
+    {location: [44.931244, -93.079578], marker: null},
+    {location: [44.956192, -93.060189], marker: null},
+    {location: [44.978883, -93.068163], marker: null},
+    {location: [44.975766, -93.113887], marker: null},
+    {location: [44.959639, -93.121271], marker: null},
+    {location: [44.947700, -93.128505], marker: null},
+    {location: [44.930276, -93.119911], marker: null},
+    {location: [44.982752, -93.147910], marker: null},
+    {location: [44.963631, -93.167548], marker: null},
+    {location: [44.973971, -93.197965], marker: null},
+    {location: [44.949043, -93.178261], marker: null},
+    {location: [44.934848, -93.176736], marker: null},
+    {location: [44.913106, -93.170779], marker: null},
+    {location: [44.937705, -93.136997], marker: null},
+    {location: [44.949203, -93.093739], marker: null}
+];
+
+function init() {
+    let crime_url = 'http://localhost:8000';
+
+    app = new Vue({
+        el: '#app',
+        data: {
+            map: {
+                center: {
+                    lat: 44.955139,
+                    lng: -93.102222,
+                    address: ""
+                },
+                zoom: 12,
+                bounds: {
+                    nw: {lat: 45.008206, lng: -93.217977},
+                    se: {lat: 44.883658, lng: -92.993787}
+                }
+            }
+        }
+    });
+
+    map = L.map('leafletmap').setView([app.map.center.lat, app.map.center.lng], app.map.zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 11,
+        maxZoom: 18
+    }).addTo(map);
+    map.setMaxBounds([[44.883658, -93.217977], [45.008206, -92.993787]]);
+    
+    let district_boundary = new L.geoJson();
+    district_boundary.addTo(map);
+
+    getJSON('data/StPaulDistrictCouncil.geojson').then((result) => {
+        // St. Paul GeoJSON
+        $(result.features).each(function(key, value) {
+            district_boundary.addData(value);
+        });
+    }).catch((error) => {
+        console.log('Error:', error);
+    });
+}
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            dataType: "json",
+            url: url,
+            success: function(data) {
+                resolve(data);
+            },
+            error: function(status, message) {
+                reject({status: status.status, message: status.statusText});
+            }
+        });
+    });
+}
---
+++ server.js
@@ -0,0 +1,95 @@
+// Built-in Node.js modules
+let path = require('path');
+
+// NPM modules
+let express = require('express');
+let sqlite3 = require('sqlite3');
+
+
+let app = express();
+let port = 8000;
+
+let public_dir = path.join(__dirname, 'public');
+let template_dir = path.join(__dirname, 'templates');
+let db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
+
+// open stpaul_crime.sqlite3 database
+// data source: https://information.stpaul.gov/Public-Safety/Crime-Incident-Report-Dataset/gppb-g9cg
+let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening ' + db_filename);
+    }
+    else {
+        console.log('Now connected to ' + db_filename);
+    }
+});
+
+app.use(express.static(public_dir));
+
+
+// REST API: GET /codes
+// Respond with list of codes and their corresponding incident type
+app.get('/codes', (req, res) => {
+    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+    
+    res.status(200).type('json').send({});
+});
+
+// REST API: GET /neighborhoods
+// Respond with list of neighborhood ids and their corresponding neighborhood name
+app.get('/neighborhoods', (req, res) => {
+    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+
+    res.status(200).type('json').send({});
+});
+
+// REST API: GET/incidents
+// Respond with list of crime incidents
+app.get('/incidents', (req, res) => {
+    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+
+    res.status(200).type('json').send({});
+});
+
+// REST API: PUT /new-incident
+// Respond with 'success' or 'error'
+app.put('/new-incident', (req, res) => {
+    let url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
+
+    res.status(200).type('txt').send('success');
+});
+
+
+// Create Promise for SQLite3 database SELECT query 
+function databaseSelect(query, params) {
+    return new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(rows);
+            }
+        })
+    })
+}
+
+// Create Promise for SQLite3 database INSERT query
+function databaseInsert(query, params) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    })
+}
+
+
+// Start server
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
